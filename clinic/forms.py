7726cec5c798c3927013056e2fdb1e2ba283c4ef
Max9222 <maxoog@bk.ru>
2024-02-02 17:00:39 +0300

    Добавил файл .env.sample Поправил ошибки кода полученные после flake8.

--- clinic/forms.py
+++ clinic/forms.py
@@ -1,11 +1,13 @@
 from django import forms
-from clinic.models import *
+from clinic.models import Blog
+
 
 class StyleFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+
 
 class BlogForm(StyleFormMixin, forms.ModelForm):
 
--- clinic/views.py
+++ clinic/views.py
@@ -1,19 +1,13 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-
-from clinic.forms import BlogForm
 from clinic.models import Clinic, Team, Area, Blog, Services
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView, TemplateView
-from django.http import Http404
+from django.views.generic import ListView, DetailView, DeleteView, TemplateView
 
 
 class ClinicListView(ListView):
     model = Clinic
 
-class MainView(TemplateView):
 
+class MainView(TemplateView):
     template_name = 'clinic/main.html'
 
 
@@ -24,22 +18,9 @@ class TeamListView(ListView):
 class AreaListView(ListView):
     model = Area
 
+
 class AreaDetailView(DetailView):
     model = Area
-
-
-
-
-class BlogCreateView(CreateView):
-    model = Blog
-    form_class = BlogForm
-    success_url = reverse_lazy('clinic:blog_list')
-
-
-class BlogUpdateView(UpdateView):
-    model = Blog
-    fields = ('title', 'content', 'preview',)
-    success_url = reverse_lazy('clinic:blog_list')
 
 
 class BlogListView(ListView):
@@ -59,12 +40,6 @@ class BlogDetailView(DetailView):
 class BlogDeleteView(DeleteView):
     model = Blog
     success_url = reverse_lazy('clinic:blog_list')
-
-    def get_object(self, queryset=None):
-        self.object = super().get_object(queryset)
-        if self.object.user != self.request.user:
-            raise Http404
-        return self.object
 
 
 class ServicesListView(ListView):
